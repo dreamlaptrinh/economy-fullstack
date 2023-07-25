05cfd2aaa4150409716d0de1cb47726c80d032a5
Dreamlaptrinh <taibooi97@gmail.com>
2023-07-25 17:41:30 +0700

    upload

---
+++ back-end/product.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id:1,
+        name: "Iphone 12 Pro",
+        desc:"6.1 - inch display",
+        price: 999,
+        image:"https://res.cloudinary.com/taipham/image/upload/v1689323951/onlineShop/iphone12pro_osw4vb.jpg"
+    },
+    {
+        id:2,
+        name: "Iphone 12",
+        desc:"5.4 - inch display",
+        price: 699,
+        image:"https://res.cloudinary.com/taipham/image/upload/v1689323947/onlineShop/iphone12_kv1zbb.jpg"
+    },
+    {
+        id:3,
+        name: "Galaxy S",
+        desc:"6.5 - inch display",
+        price: 399,
+        image:"https://res.cloudinary.com/taipham/image/upload/v1689323936/onlineShop/galaxyS_rajfos.png"
+    }
+];
+export default products;
